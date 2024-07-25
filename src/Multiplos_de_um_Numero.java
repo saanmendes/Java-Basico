7448895c116e3f013cefbb2cf5045011976d92fb
Sandra Mendes <sandra.mendes@gmail.com>
2024-07-25 14:39:58 -0300

    multiples of a number

---
+++ src/Multiplos_de_um_Numero.java
@@ -0,0 +1,13 @@
+public class Multiplos_de_um_Numero {
+    public static void main(String[] args) {
+        int aux=1;
+        int n = 3;
+        int m= 15;
+
+        for(int i = 1; i <= 3;){
+            aux = aux * n;
+        }
+
+        System.out.println("multiplos de três são" + n );
+    }
+}
