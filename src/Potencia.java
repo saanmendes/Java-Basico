054ca29744e0fb75a1c767d2b5a324ceb1d24f7c
Sandra Mendes <sandra.mendes@gmail.com>
2024-07-25 14:42:12 -0300

    power

---
+++ src/Potencia.java
@@ -0,0 +1,15 @@
+
+
+public class Potencia {
+    public static void main(String[] args) {
+        int base = 2;
+        int expoente = 3;
+        int potencia = 1;
+
+        for (int i = 0; i < 3; i++) {
+           potencia = potencia * base;
+
+        }
+        System.out.print("o resultado é "+potencia);
+    }
+}
